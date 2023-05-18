ff28068e20670a04788651d2f07c3ece611df6a5
diegobomfimx <diegobomfimx@gmail.com>
2023-05-18 18:26:53 -0300

    import positions

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 import initAcordeon from "./modules/acordeon-anime.js";
 import initTabNav from "./modules/tab-nav.js";
 import ScrollSuave from "./modules/scroll-suave.js";
-import initAnimacaoScroll from "./modules/animacao-scroll.js";
 import initModal from "./modules/modal.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initBtcPreco from "./modules/fetch-bitcoin.js";
+import initAnimacaoScroll from "./modules/animacao-scroll.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
 initAcordeon();
 initTabNav();
-initAnimacaoScroll();
 initModal();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initBtcPreco();
+initAnimacaoScroll();
