e6270c4e549d884e803e8130719e3a7caed40db4
diegobomfimx <diegobomfimx@gmail.com>
2023-05-18 16:03:41 -0300

    scrollSuave transform  into class

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import initAcordeon from "./modules/acordeon-anime.js";
 import initTabNav from "./modules/tab-nav.js";
-import initScrollSuave from "./modules/scroll-suave.js";
+import ScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/animacao-scroll.js";
 import initModal from "./modules/modal.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
@@ -9,9 +9,11 @@ import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initBtcPreco from "./modules/fetch-bitcoin.js";
 
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
+
 initAcordeon();
 initTabNav();
-initScrollSuave();
 initAnimacaoScroll();
 initModal();
 initDropdownMenu();
