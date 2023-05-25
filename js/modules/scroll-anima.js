24d71c3c498081f44d3e1be2ca98a97d918031a2
diegobomfimx <diegobomfimx@gmail.com>
2023-05-25 13:47:19 -0300

    refatorar class scroll-anima

---
+++ js/modules/scroll-anima.js
@@ -0,0 +1,22 @@
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.8;
+    this.animaScroll = this.animaScroll.bind(this);
+  }
+
+  animaScroll() {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - this.windowMetade < 0;
+      if (isSectionVisible) {
+        section.classList.add("ativo");
+      }
+    });
+  }
+
+  init() {
+    this.animaScroll();
+    window.addEventListener("scroll", this.animaScroll);
+  }
+}
